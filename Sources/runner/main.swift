c1bae58daca34736f7bc68fc2059eae9cf195906
Jerzy Chalupski <jerzy.chalupski@ig.com>
2019-12-03 17:02:43 +0100

    Add elapsed time measurement to runner

--- Sources/runner/main.swift
+++ Sources/runner/main.swift
@@ -2,6 +2,14 @@ import Foundation
 
 import advent2019
 
+func advent(day: Int, task: Int, block: () throws -> Any) rethrows {
+    let start = Date()
+    let result = try block()
+    let elapsedMs: Int = Int(-start.timeIntervalSinceNow * 1_000)
+    
+    print("Day \(day), task \(task) [\(elapsedMs)ms]: \(result)")
+}
+
 do {
     let dir = CommandLine.arguments[1]
 
@@ -9,13 +17,12 @@ do {
     let day2 = try String(contentsOfFile: dir + "day2.txt").singleLine()
     let day3 = try String(contentsOfFile: dir + "day3.txt").lines()
 
-    
-    print("Day 1, task 1: \(day1task1(input: day1))")
-    print("Day 1, task 2: \(day1task2(input: day1))")
-    print("Day 2, task 1: \(try day2task1(input: day2))")
-    print("Day 2, task 2: \(try day2task2(input: day2, desiredOutput: 19690720))")
-    print("Day 3, task 1: \(try distanceToNearestIntersection(wireSpecs: day3))")
-    print("Day 3, task 2: \(try shortestIntersectionPath(wireSpecs: day3))")
+    advent(day: 1, task: 1) { day1task1(input: day1) }
+    advent(day: 1, task: 2) { day1task2(input: day1) }
+    try advent(day: 2, task: 1) { try day2task1(input: day2) }
+    try advent(day: 2, task: 2) { try day2task2(input: day2, desiredOutput: 19690720) }
+    try advent(day: 3, task: 1) { try distanceToNearestIntersection(wireSpecs: day3) }
+    try advent(day: 3, task: 2) { try shortestIntersectionPath(wireSpecs: day3) }
 } catch {
     print("uh oh :( \(error)")
 }
