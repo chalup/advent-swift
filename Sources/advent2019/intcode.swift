1957539a99c96c748d9b8f48e9cd3d6ff60a8d18
Jerzy Chalupski <jerzy.chalupski@ig.com>
2019-12-06 10:01:16 +0100

    More refactoring

--- Sources/advent2019/intcode.swift
+++ Sources/advent2019/intcode.swift
@@ -9,25 +9,46 @@ enum ProgramResult {
     case executionError(error: InterpreterError)
 }
 
+enum InterpreterStatus {
+    case Running
+    case Halted
+    case Error(error: InterpreterError)
+}
+
+struct State {
+    var ip = 0
+    var memory: [Int]
+    var status: InterpreterStatus = InterpreterStatus.Running
+    
+    init(program: [Int]) {
+        memory = program
+    }
+    
+}
+
 class IntcodeInterpreter {
     static func execute(_ program: [Int]) -> ProgramResult {
-        var memory = program
-        var ip = 0
+        var state = State(program: program)
         
         while true {
-            let opcode = memory[ip]
-            
-            switch opcode {
-            case 1:
-                memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]]
-                ip += 4
-            case 2:
-                memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]]
-                ip += 4
-            case 99:
-                return ProgramResult.finished(finalState: memory)
-            default:
-                return ProgramResult.executionError(error: InterpreterError.invalidOpcode(ip: ip, opcode: opcode, dump: memory))
+            switch state.status {
+            case .Running:
+                let opcode = state.memory[state.ip]
+                
+                switch opcode {
+                case 1:
+                    state.memory[state.memory[state.ip + 3]] = state.memory[state.memory[state.ip + 1]] + state.memory[state.memory[state.ip + 2]]
+                    state.ip += 4
+                case 2:
+                    state.memory[state.memory[state.ip + 3]] = state.memory[state.memory[state.ip + 1]] * state.memory[state.memory[state.ip + 2]]
+                    state.ip += 4
+                case 99:
+                    state.status = InterpreterStatus.Halted
+                default:
+                    state.status = InterpreterStatus.Error(error: InterpreterError.invalidOpcode(ip: state.ip, opcode: opcode, dump: state.memory))
+                }
+            case .Halted: return ProgramResult.finished(finalState: state.memory)
+            case .Error(let error): return ProgramResult.executionError(error: error)
             }
         }
     }
